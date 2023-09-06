7d986ba08e3b5836414813c8aed1aff87d97798c
AntonKovalenk0 <55621773+AntonKovalenk0@users.noreply.github.com>
2023-09-06 21:10:12 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss'
+import HeaderBlock from './pages/header/HeaderBlock'
+
+function App() {
+  return (
+    <div className={styles.Background}>
+        <HeaderBlock></HeaderBlock>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/header/HeaderBlock.jsx
@@ -0,0 +1,11 @@
+import styles from './HeaderBlock.module.scss';
+
+function HeaderBlock() {
+    return (
+      <div className={styles.Background}>
+
+      </div>
+    );
+  }
+
+export default HeaderBlock;
