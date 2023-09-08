a68e0dde2ceeffe7d3d3e5b10b7a39f64591f43f
15Arasei <95213823+15Arasei@users.noreply.github.com>
2023-09-08 11:05:25 +0300

    Update HeaderBlock.jsx

--- src/pages/header/HeaderBlock.jsx
+++ src/pages/header/HeaderBlock.jsx
@@ -1,6 +1,26 @@
+import Button from '../../UI/button/Button';
 import styles from './HeaderBlock.module.scss';
 
 function HeaderBlock() {
+
+  const buttonsConfig = [
+    {
+      name: 'Про нас',
+      href: '#.....',
+      type: 'link',
+    },
+    {
+      name: 'Послуги',
+      href: '#.....',
+      type: 'link',
+    },
+    {
+      name: 'Контакти',
+      href: '#.....',
+      type: 'link',
+    },
+  ];
+
     return (
       <div className={styles.Background}>
           <div className={styles.FixationBlock}>
@@ -11,7 +31,15 @@ function HeaderBlock() {
               </a>
             </div>
             <div className={styles.MenuButtons}>
-
+              {buttonsConfig.map(({ name, href, onClick }) => (
+                <Button
+                  key={name}
+                  type="link"
+                  name={name}
+                  href={href}
+                  onClick={onClick}
+                />
+              ))}
             </div>
             <div className={styles.LanguageBlock}>
 
