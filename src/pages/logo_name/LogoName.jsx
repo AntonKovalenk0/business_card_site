64169914d53082e0aeefa387789e4d5e9ce7717b
15Arasei <95213823+15Arasei@users.noreply.github.com>
2023-09-11 16:37:53 +0300

    Update LogoName.jsx

--- src/pages/logo_name/LogoName.jsx
+++ src/pages/logo_name/LogoName.jsx
@@ -2,29 +2,25 @@ import styles from './LogoName.module.scss';
 
 function LogoName() {
     return (
-      <div className={styles.Background}>
-          <div className={styles.FixationBlock}>
-            
-            <div className={styles.Logo}>
+        <div className={styles.Background}>
 
-            </div>
-
-            <div className={styles.NameContent}>
-                <div className={styles.Name}>
-                  <div className={styles.FirstName}>
-                    <span>LEOWAVE</span>
-                    <span>LEOWAVE</span>
-                  </div>
-                <span className={styles.LastName}>innovations</span>
-                </div>
-                <div className={styles.Buttons}>
-
-                </div>
-
-            </div>
+          <div className={styles.Logo}>
 
           </div>
-      </div>
+
+          <div className={styles.NameContent}>
+              <div className={styles.Name}>
+                <div className={styles.FirstName}>
+                  <span>LEOWAVE</span>
+                  <span>LEOWAVE</span>
+                </div>
+                <div className={styles.LastName}>innovations</div>
+              </div>
+              <div className={styles.Logo}>
+
+              </div>
+          </div>
+        </div>
     );
   }
 
